dbd1ac256f8fe7c3d62ae3b84924d7e9f5e0a55b
kelsonPadilha <kelson.padilha@gmail.com>
2025-11-13 02:47:51 +0000

    for, do, while

---
+++ # include <stdio.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main () {
+
+int numero, i;
+
+printf("digite o numero\n");
+scanf("%d", &numero);
+
+for (i = 0; i <= 10; i++)
+{
+    printf("%d x %d = %d\n", i, numero, i * numero);    
+}
+
+
+
+    return 0;
+}
